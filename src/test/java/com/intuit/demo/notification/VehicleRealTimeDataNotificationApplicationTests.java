c9edac14f3f378960f222554e745de447332c89a
Sunstreaker <suthagar.pp@gmail.com>
2023-10-11 21:21:46 +0530

    Update VehicleRealTimeDataNotificationApplicationTests.java

--- src/test/java/com/intuit/demo/notification/VehicleRealTimeDataNotificationApplicationTests.java
+++ src/test/java/com/intuit/demo/notification/VehicleRealTimeDataNotificationApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VehicleRealTimeDataNotificationApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
